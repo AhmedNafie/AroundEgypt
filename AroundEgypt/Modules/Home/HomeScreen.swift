c242be14671c8e2143bcb9303ab5deb89c43f1a5
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2024-12-05 18:03:10 +0200

    Add HomeScreen

--- AroundEgypt/Modules/Home/HomeScreen.swift
+++ AroundEgypt/Modules/Home/HomeScreen.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeScreen.swift
 //  AroundEgypt
 //
 //  Created by Ahmed Nafie on 05/12/2024.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeScreen: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeScreen()
 }

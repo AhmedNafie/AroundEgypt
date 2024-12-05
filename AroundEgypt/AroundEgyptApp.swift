5b1fdc93a93d9670300c63ac845ebec92c9ce43c
Ahmed Nafie <Ahmedtareknafie@gmail.com>
2024-12-05 16:48:51 +0200

    Initial Commit

---
+++ AroundEgypt/AroundEgyptApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptApp.swift
+//  AroundEgypt
+//
+//  Created by Ahmed Nafie on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
